b54c8ebea91f12c435d2229999dd3caf22385ab7
Kevin Gutierrez <mzn20043@yahoo.com>
2016-11-01 16:40:55 -0400

    Moved timeline label into bar & topic column removed

--- src/main/webapp/resources/js/timeline.js
+++ src/main/webapp/resources/js/timeline.js
@@ -3,20 +3,38 @@ var app = angular.module('batchApp');
 /*--------------------------CONTROLLER---------------------------*/
 
 app.controller("TimelineCtrl", function($scope, allBatchService){
-	//Timeline x axis range variables
-	var minDate = new Date(2016,7,20),
-		maxDate = new Date(2017,11,31);
-
+	
+	//Options for datepicker
+	$scope.options = {
+		datepickerMode: "month",
+		minMode: "month"
+	}
+	
+	//Timeline axis range variables
+	$scope.minDate = new Date(2016,7,0);
+	$scope.maxDate = new Date(2017,12,0);
+	
 	//Pull batch data from service
+	$scope.data;
 	allBatchService.getAllBatches(function(response){
-		if (response.data !== undefined){
-			var data = response.data;
-			projectTimeline(minDate, maxDate, data);
+		if (response !== undefined){
+			$scope.data = response.data;
+			projectTimeline($scope.minDate, $scope.maxDate, $scope.data);
 		}
 	});
 	
+	//Project new timeline when min or max date changes
+	$scope.$watch( 'minDate', function(){
+		if($scope.data !== undefined){
+			projectTimeline($scope.minDate, $scope.maxDate, $scope.data);
+		}
+	});
 	
-
+	$scope.$watch('maxDate', function(){
+		if($scope.data !== undefined){
+			projectTimeline($scope.minDate, $scope.maxDate, $scope.data);
+		}
+	});
 });
 
 //Determine number of weeks in a batch
@@ -26,7 +44,7 @@ function numWeeks(date1, date2) {
     var date1ms = new Date(date1).getTime();
     var date2ms = new Date(date2).getTime();
 
-    var diff = Math.abs(date2ms - date1ms);
+    var diff = date2ms - date1ms;
 
     return Math.floor(diff / week);
 };
@@ -34,11 +52,10 @@ function numWeeks(date1, date2) {
 function projectTimeline(minDate, maxDate, timelineData){
 	
 	//Timeline variables
-	var padding = {top: 20, right: 10, bottom: 30, left:75},
-	width = 1700 - padding.left - padding.right,
-	height = 2000 - padding.top - padding.bottom,
-	margin = 50,
-	barWidth = 30;
+	var margin = {top: 30, right: 10, bottom: 30, left:75},
+	width = 1700 - margin.left - margin.right,
+	height = 2000 - margin.top - margin.bottom,
+	xPadding = 50;
 
 	//Define Scales
 	var colorScale = d3.scale.category20();
@@ -49,7 +66,7 @@ function projectTimeline(minDate, maxDate, timelineData){
 	
 	var xScale = d3.scale.ordinal()
 		.domain(['August(Java)','Fred(.NET)','Joe(.NET)','Brian(Java)','Taylor(Java)','Patrick(Java)','Yuvi(SDET)','Steven(Java)','Ryan(SDET)','Richard(Java)','Nicholas(Java)','Ankit(Java)','Genesis(Java)','Emily(.NET)'])
-		.rangePoints([margin,width-margin]);
+		.rangePoints([xPadding,width-xPadding]);
 	
 	//Define axis
 	var yAxis = d3.svg.axis()
@@ -60,15 +77,20 @@ function projectTimeline(minDate, maxDate, timelineData){
 	var xAxis = d3.svg.axis()
 		.scale(xScale)
 		.orient('top')
-		.tickSize(0);
+		.tickSize(6,0);
 	
 	//Create timeline
+	d3.select('svg').remove();
+	
+	console.log('height:'+height);
+	timelineData.forEach(function(d){console.log(yScale(new Date(d.batchEndDate)));});
+	
 	var svg = d3.select('#timeline')
 		.append('svg')
-			.attr('width',width+padding.left+padding.right)
-			.attr('height',height+padding.top+padding.bottom)
+			.attr('width',width + margin.left + margin.right)
+			.attr('height',height + margin.bottom + margin.top)
 		.append('g')
-			.attr('transform','translate('+padding.left+','+padding.top+')')
+			.attr('transform','translate('+margin.left+','+margin.top+')');
 			
 	svg.append('g')
 		.attr('class','x axis')
@@ -77,6 +99,9 @@ function projectTimeline(minDate, maxDate, timelineData){
 	svg.append('g')
 		.attr('class','y axis')
 		.call(yAxis);
+	
+//	svg.append('g')
+//		.attr('class','swimlanes');
 	
 	svg.append('g')
 		.attr('class','rectangles');
@@ -88,14 +113,36 @@ function projectTimeline(minDate, maxDate, timelineData){
 		.append('g')
 			.attr('class','rect')
 		.append('rect')
-			.attr('y', function(d) {return yScale(new Date(d.batchStartDate));})
+			.attr('y', function(d) {
+				var y = yScale(new Date(d.batchStartDate));
+				if (y < 0){
+					y = 0;
+				}
+				return y;
+			})
 			.attr('x', function(d) {return xScale(d.batchTrainerID.trainerFirstName+"("+d.batchCurriculumID.curriculumName+")")-10;})
-			.attr('width', barWidth)
-			.attr('height', function(d) {return yScale(new Date(d.batchEndDate)) - yScale(new Date(d.batchStartDate));})
+			.attr('width', 30)
+			.attr('height', function(d) {
+				var start = yScale(new Date(d.batchStartDate));
+				var end = yScale(new Date(d.batchEndDate));
+				if (start < 0){
+					start = 0;
+				}
+				if(end > height){
+					end = 1940;
+				}
+				return end - start;
+			})
 			.style('fill', function(d) {return colorScale(d.batchTrainerID.trainerFirstName);});
 	d3.selectAll('.rect')
 		.append('text')
-			.attr('y', function(d) {return yScale(new Date(d.batchStartDate))-5;})
+			.attr('y', function(d) { 
+				var y = yScale(new Date(d.batchStartDate));
+				if (y < 0){
+					y = 0;
+				}
+				return (y+25);
+			})
 			.attr('x', function(d) {return xScale(d.batchTrainerID.trainerFirstName+"("+d.batchCurriculumID.curriculumName+")")-2;})
 			.text(function(d) {return numWeeks(d.batchStartDate,d.batchEndDate);});
 };
