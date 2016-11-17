c3f1964d19ef14b7e1b5677b932ae29c209d629d
August Duet <august@revature.com>
2016-11-17 11:54:50 -0500

    changed beans

--- src/main/java/com/ateam/controllers/LoadCtrl.java
+++ src/main/java/com/ateam/controllers/LoadCtrl.java
@@ -15,6 +15,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
+
 public class LoadCtrl {
 	
 	
@@ -34,12 +35,6 @@ public class LoadCtrl {
 		String userName = obj.get("username").textValue();
 		String password = obj.get("password").textValue();
 		
-//		if((!userName.equals("ravi.singh") || !password.equals("@revature_ravi_singh!")) ||
-//				(!userName.equals("rev.dev") || !password.equals("@rev_dev!"))){
-//			return new ResponseEntity<Boolean>(false, HttpStatus.UNAUTHORIZED);
-//		}else{
-//			return new ResponseEntity<Boolean>(true, HttpStatus.OK);
-//		}
 		
 		if("ravi.singh".equals(userName) && "@revature_ravi_singh!".equals(password)){
 			return new ResponseEntity<Boolean>(true, HttpStatus.OK);
@@ -48,27 +43,6 @@ public class LoadCtrl {
 		}else{
 			return new ResponseEntity<Boolean>(false, HttpStatus.UNAUTHORIZED);
 		}
-		
-//		//extract the authorization code
-//		String authCode = request.getParameter("code");
-//		SFWSAccessor sfAccessor = new SFWSAccessor();
-//		SFWSAccessObject sfAccess;
-//
-//		//client keys
-//		String clientId = "3MVG9bx.kiqxiA6ZMQErdHzfMtKe8sqAvAo6Waq2eqsHE9xE_B0TwJZ65npFtCDp7kJK7Q8IpCUO0EnUKWc5b"; //required for salesforce as the consumer key;
-//		String redirect = "https://dev.aduet.tech"; //required by salesforce for calling back to the app
-//		String clientSecret = "2162566647397604584";
-//
-//		//if authCode is there the the user has logged in
-//		//but we still need to get an access token
-//		//if authCode is null we need to start the login process
-//
-//		if (authCode == null) {
-//			response.sendRedirect(sfAccessor.getAuthorizationUrl(clientId, redirect, null));
-//		} else {
-//			//GET THE ACCESS TOKEN
-//			sfAccess = sfAccessor.getAccessToken(clientId, clientSecret, redirect, authCode, null);
-//		}
 	}
 	
 	
