91ddf334ef2d516f8027d3f801c3c6b7f3f35e59
Kevin Gutierrez <mzn20043@yahoo.com>
2016-11-30 16:36:42 -0500

    Somewhat sales force integration v2

--- src/main/resources/public/js/controllers/authController.js
+++ src/main/resources/public/js/controllers/authController.js
@@ -7,11 +7,13 @@ var app = angular.module("batchApp");
 app.controller("AuthCtrl", function($scope, $location, $window){
 
 	$scope.$on('$viewContentLoaded', function(){
-		if($location.search().token){
-			$window.sessionStorage.setItem('token',$location.search().token)
-		}
-		else {
-			$window.location.replace('https://sf.aduet.tech/services/auth?redirect_url=http%3A%2F%2Fdev2.aduet.tech/home');
+		if(!$window.sessionStorage.getItem('token')){
+            if($location.search().token){
+                $window.sessionStorage.setItem('token',$location.search().token)
+            }
+            else {
+                $window.location.replace('https://sf.aduet.tech/services/auth?redirect_url=http%3A%2F%2Fdev2.aduet.tech/home');
+            }
 		}
 	});
 
