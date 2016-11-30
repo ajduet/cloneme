0fd3998f712bee41a3a22a86fa55880754652df2
August Duet <august@revature.com>
2016-11-30 15:03:52 -0500

    Add SFA integration

--- src/main/java/com/revature/assignforce/AssignForceV2Application.java
+++ src/main/java/com/revature/assignforce/AssignForceV2Application.java
@@ -1,14 +1,9 @@
 package com.revature.assignforce;
 
-import com.revature.assignforce.util.HttpSessionCollector;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import javax.servlet.http.HttpSessionListener;
 
 /**
  * Application Entry Point
@@ -18,16 +13,8 @@ import javax.servlet.http.HttpSessionListener;
 @EntityScan("com.revature.assignforce.domain")
 public class AssignForceV2Application {
 
-	@Autowired
-	private HttpSessionCollector sessions;
-
 	public static void main(String[] args) {
 		SpringApplication.run(AssignForceV2Application.class, args);
 	}
 
-	@Bean
-	HttpSessionListener httpSessionListener(){
-		System.out.println("Returning our sessions");
-		return sessions;
-	}
 }
--- src/main/java/com/revature/assignforce/web/AuthController.java
+++ src/main/java/com/revature/assignforce/web/AuthController.java
@@ -1,24 +1,13 @@
 package com.revature.assignforce.web;
 
-import java.io.IOException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.assignforce.service.AuthService;
-import com.revature.assignforce.util.HttpSessionCollector;
-import io.jsonwebtoken.lang.Assert;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionListener;
+import java.io.IOException;
 
 @RestController
 @RequestMapping(value = "/api/v2")
@@ -30,12 +19,15 @@ public class AuthController {
 	@RequestMapping(value="/authorize",method=RequestMethod.GET)
 	public void initSetup(@RequestParam String redirect_url, HttpSession session, HttpServletResponse response) throws IOException{
 		System.out.println(session.getId());
-//		if(sToken == null){
-//			response.sendRedirect(String.format("https://sf.aduet.tech/services/auth?redirect_url=http://dev2.aduet.tech/api/v2/token?id=%s", request.getSession().getId()));
-//		}
+		String sToken = (String) session.getAttribute("token");
+
+		if(sToken == null){
+			response.sendRedirect("https://sf.aduet.tech/services/auth?redirect_url=http://dev2.aduet.tech/api/v2/token");
+		}
 	}
 	
 	@RequestMapping(value="/token")
-	public void getToken(@RequestParam(required = false) String token, @RequestParam(required = false) String id, HttpServletResponse response) throws IOException {
+	public void getToken(@RequestParam(required = false) String token, HttpSession session, HttpServletResponse response) throws IOException {
+		System.out.println(session.getId());
 	}
 }
